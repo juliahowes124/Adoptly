4fcdb9d8f962dedf0bd5411708e5ac98d4957391
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-03-12 17:01:21 -0800

    api section

--- app.py
+++ app.py
@@ -2,11 +2,17 @@
 
 from flask import Flask, render_template, redirect, flash
 
+import requests
+
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Pet
 
 from forms import AddPetForm
+
+from projects_secrets import PETFINDER_API_KEY, PETFINDER_SECRET_KEY
+
+from random import choice
 
 app = Flask(__name__)
 
@@ -25,6 +31,26 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+
+def get_auth_token():
+    resp = requests.post("https://api.petfinder.com/v2/oauth2/token",
+                        data={"grant_type": "client_credentials",
+                                "client_id": PETFINDER_API_KEY,
+                                "client_secret": PETFINDER_SECRET_KEY})
+    print(resp)
+    # token = resp.json()["access_token"]
+    return resp.json()
+
+
+def get_random_pet():
+    token = get_auth_token()
+    resp = requests.get("/https://api.petfinder.com/v2/animals?limit=100",
+                        headers={"Authorization": f"Bearer {token}"})
+    print(resp.json())
+    random_pet = choice(resp.json().items)
+    return random_pet
+    
 
 
 @app.route('/')
